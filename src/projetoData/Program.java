9b1980f7e2026975c55d697d61d4793ac57788b6
Fernando <fernando.luiz92@hotmail.com>
2022-02-10 23:49:20 -0300

    Project created

---
+++ src/projetoData/Program.java
@@ -0,0 +1,33 @@
+package projetoData;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		List<String> listaDatas = new ArrayList<String>();
+		
+		String periodoMassa0 = "04/08/2018";
+		String periodoMassa1 = "05/08/2018";
+		String periodoInicia = "06/08/2018";
+		String periodoMassa2 = "07/08/2018";
+		String periodoMassa3 = "08/08/2018";
+		String periodoMassa4 = "09/08/2018";
+		String periodoFinal = "10/08/2018";
+		String periodoMassa6= "11/08/2018";
+		
+		listaDatas.add(periodoMassa0);
+		listaDatas.add(periodoMassa1);
+		listaDatas.add(periodoInicia);
+		listaDatas.add(periodoMassa2);
+		listaDatas.add(periodoMassa3);
+		listaDatas.add(periodoMassa4);
+		listaDatas.add(periodoFinal);
+		listaDatas.add(periodoMassa6);
+
+		
+	}
+}
